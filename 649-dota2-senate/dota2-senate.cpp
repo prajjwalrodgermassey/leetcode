8c443c4f15c58df322ea1a1b5f24425e1887ca18
prajjwalrodgermassey <99514751+prajjwalrodgermassey@users.noreply.github.com>
2025-11-04 13:02:44 +0530

    Time: 349 ms (5.04%) | Memory: 11.5 MB (67.67%) - LeetSync

--- 649-dota2-senate/dota2-senate.cpp
+++ 649-dota2-senate/dota2-senate.cpp
@@ -8,9 +8,7 @@ public:
     string predictPartyVictory(string senate) {
         std::queue<int> r_index_queue, d_index_queue;
 
-        if(senate.length() == 0){
-            return "";
-        }
+   
 /* Here we store indices of the R's and the D's into the qeueue which are taken from the given string*/
         for(int i = 0; i < senate.length(); i++){
             if(senate[i] == 'R'){
@@ -20,12 +18,6 @@ public:
             }
         }
 
-//Here we check if any one of the queue is empty we early return the other winner
-            if(d_index_queue.empty()){
-                return "Radiant";
-            }else if(r_index_queue.empty()){
-                return "Dire";
-            }
 
 /* Here we run our loops over the 2 queues together simultaneously which gives an impression if the index of R 
 is smaller than the index of D it means that R appeared in front of the D and thus can override the powers of the D and vice versa we also insert the respective index of R or D whichever is smaller again into their respective index queue as this will ensure that the winner is decided round after round we increment the index by the length of the given string and insert it into the queue making sure if there is an R or D which can override the other if one appears at the end.
@@ -44,12 +36,13 @@ is smaller than the index of D it means that R appeared in front of the D and th
                 d_index_queue.push(d_index + senate.length());
             }
 
+        }
+       
+//Here we check if any one of the queue is empty we return the other winner
             if(d_index_queue.empty()){
                 return "Radiant";
-            }else if(r_index_queue.empty()){
+            }else{
                 return "Dire";
             }
-        }
-        return "";
     }
 };
